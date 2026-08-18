71fddabfa00f14aaca68acd1bb660d3a75c814a7
6jn2 <mohammadmojib901@gmail.com>
2026-08-18 20:12:42 +0300

    Fix: nixpacks php -S server, CORS, admin seeder

--- config/cors.php
+++ config/cors.php
@@ -4,9 +4,9 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => ['#.*localhost.*#'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['Authorization'],
     'max_age' => 86400,
-    'supports_credentials' => false, // false when using Bearer token (not cookie)
+    'supports_credentials' => false,
 ];
